34117e29af8c27e5ed737adb4410d93d9836284c
Dan Reynolds <dreynolds2712@gmail.com>
2021-01-04 19:39:02 +0100

    remove crop

--- discord_bot/crop.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import shlex
-
-
-ffmpeg_options = f"-vn -i bugsnax.webm -ss 10 -to 15 -c copy output.webm -y"
-args = ["ffmpeg"]
-args.extend(shlex.split(ffmpeg_options))
-
-subprocess.run(args)
