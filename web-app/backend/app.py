f43de90b4bd865d0f6831788b90556059c0a72e1
Dan Reynolds <dreynolds2712@gmail.com>
2021-08-03 23:12:37 +0200

    Rabbitmq integration

--- web-app/backend/app.py
+++ web-app/backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, Blueprint
 import os
+import time
+import pika
+import asyncio
+from threading import Thread
+
 
 # import os
 # from steelforge_site_utils.aws_verify import verify_jwt, authed, get_user_type, verify_access_token
@@ -9,7 +14,7 @@ SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
 app = Flask(__name__)
 # app.secret_key = SECRET_KEY
 dcoops = Blueprint("dcoops", __name__)
-app.register_blueprint(dcoops, url_prefix="/dcoops")
+
 
 print("Started")
 
@@ -21,4 +26,48 @@ def hello_world():
 
 @dcoops.route("/ping", methods=["GET"])
 def ping():
-    return "ping"
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return f"ping - {current_time}"
+
+@dcoops.route("/groans", methods=["GET"])
+def groans():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    send_rabbit()
+    return f"groans - {current_time}"
+
+
+
+
+def connection():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitMQ'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+def get_connection():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitMQ'))
+    return conn
+
+def send_rabbit():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    connection = get_connection()
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+if __name__=="__main__":
+    app.run()
+    
+app.register_blueprint(dcoops, url_prefix="/dcoops")
---
+++ web-app/backend/config.py
@@ -0,0 +1,4 @@
+FLASK_PIKA_PARAMS = {
+    'host':'rabbitMQ',      #amqp.server.com
+    'port': 5672,            #amqp server port
+}
